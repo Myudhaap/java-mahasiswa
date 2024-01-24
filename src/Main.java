7a3035cc8feb7e785b7547ce27c2d7cc36aa91ac
yudhaPratama <muhyudhapratama55@gmail.com>
2024-01-24 20:59:19 +0700

    [FEATURE] initialize project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-
-    System.out.println("Line 2");
+    System.out.println("Sistem Informasi Mahasiswa");
   }
 }
